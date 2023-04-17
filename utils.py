739e641fd132e7cea5f64dcd9005a9b99c3f5b6d
fspatola <62903636+federicsp@users.noreply.github.com>
2023-04-17 14:27:38 +0200

    small fix

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import random
 import signal
 from pygame import mixer
 
+def timeout_handler(signum, frame):
+    raise Exception("Input timeout")  # raises an exception to break the input prompt
 
 def create_audio(name, path):
     # The text that you want to convert to audio
@@ -20,14 +22,14 @@ def play_audio():
     mixer.music.play()
 
 def have_clocked_in():
-    signal.signal(signal.SIGALRM, Exception)  # register the timeout handler for SIGALRM
-
     try:
+        signal.signal(signal.SIGALRM, timeout_handler)  # register the timeout handler for SIGALRM
         signal.alarm(10)  # set the alarm to trigger after 10 seconds
         print("Input something if you have clocked in: ")
         input_data = input()
     except:
         input_data = False
+    signal.alarm(0)  # cancel the alarm
     if input_data:
         return True
     else:
